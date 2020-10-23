61857a27a084d2dad21009cd95ab4e1a04a5fcfd
Raleigh Wayland <raleigh.wayland@stormapp.com>
2020-10-22 23:43:03 -0600

    Setting up Opus keyboard

---
+++ firmware/keyboards/opus/keymaps/default/keymap.cpp
@@ -0,0 +1,130 @@
+/*
+Copyright 2020 <rtwayland>
+
+3-Clause BSD License
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include "keymap.h"
+
+std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix = {
+    KEYMAP(
+        MEDIATAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, TD_MPLY_DND,
+        CTRLESC, KC_A, KC_S, KC_D, NUMF, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, MEDIAQUOT,
+        L_NUM, GUIZ, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, GUISLSH, L_SYM,
+        L_NAV, KC_LSFT, ALTTAB, KC_ENT, SYMSPC, KC_BSPC
+    )
+};
+
+void setupKeymap() {
+    uint32_t qwerty_tap[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, TD_MPLY_DND_TAP,
+        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+                                            _______, _______, _______,    _______, _______, _______
+    );
+
+    uint32_t qwerty_dtap[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+        _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, TD_MPLY_DND_DTAP,
+        _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+                                            _______, _______, _______,    _______, _______, _______
+    );
+
+    uint32_t qwerty_taph[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+        MEDIATAB_TAP, _______, _______, _______, _______, _______,                              _______, _______, _______, _______, _______, _______,
+        CTRLESC_TAP, _______, _______, _______, NUMF_TAP, _______,                              _______, _______, _______, _______, _______, MEDIAQUOT_TAP,
+        _______, GUIZ_TAP, _______, _______, _______, _______,                              _______, _______, _______, _______, GUISLSH_TAP, _______,
+                                                _______, _______, _______,    _______, SYMSPC_TAP, _______
+    );
+
+    uint32_t qwerty_hold[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+        MEDIATAB_HOLD, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
+        CTRLESC_HOLD, _______, _______, _______, NUMF_HOLD, _______,                    _______, _______, _______, _______, _______, MEDIAQUOT_HOLD,
+        _______, GUIZ_HOLD, _______, _______, _______, _______,                       _______, _______, _______, _______, GUISLSH_HOLD, _______,
+                                                _______, _______, _______,    _______, SYMSPC_HOLD, _______
+    );
+
+    uint32_t symbol_layer[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+        CTL_C, KC_TILD, KC_AT, KC_LCBR, KC_RCBR, KC_HASH, KC_ASTR, KC_CIRC, KC_AMPR, KC_PIPE, KC_BSLS, CTL_GRV,
+        KC_LCTL, KC_GRV, KC_MINS, KC_LPRN, KC_RPRN, KC_EXLM, KC_QUES, KC_DLR, KC_LT, KC_GT, KC_SLSH, KC_DQUO,
+        KC_LALT, KC_LGUI, KC_UNDS, KC_LBRC, KC_RBRC, KC_PLUS, KC_P0, KC_EQL, KC_PERC, KC_COLN, KC_NO, KC_NO,
+        KC_NO, KC_LSFT, KC_LALT, KC_NO, KC_TRNS, KC_NO
+    );
+
+    uint32_t num_layer[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_ASTR, KC_7, KC_8, KC_9, KC_SLSH, KC_EQL,
+        KC_LCTL, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_PLUS, KC_4, KC_5, KC_6, KC_MINS, KC_PERC,
+        KC_LALT, KC_LGUI, KC_NO, KC_NO, KC_NO, KC_NO,  KC_0, KC_1, KC_2, KC_3, KC_DOT, KC_PENT,
+        KC_NO, KC_LSFT, KC_TAB, KC_NO, KC_SPC, KC_BSPC
+    );
+
+    uint32_t nav_layer[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+        KC_NO, KC_NO, NXT_WORD, PRV_TAB, NXT_TAB, KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, PRV_WORD, KC_END, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_SPC, KC_BSPC
+    );
+
+    uint32_t media_layer[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+        KC_TRNS, SCRN, SCRN_WHL, SCRN_SLCT, SCRN_SLCT_CP, KC_VOLU, UP_LEFT_CNR, PRV_SCRN, NXT_SCRN, UP_RGHT_CNR, WND_LRG, SLEEP,
+        KC_CAPS, KC_F11, KC_MPRV, KC_MNXT, KC_MPLY, KC_VOLD, WND_LEFT, WND_DOWN, WND_UP, WND_RGHT, WND_SML, KC_TRNS,
+        KC_BRID, KC_BRIU, ZOOM_OUT, ZOOM_IN, NXT_WND, KC_MUTE, BTM_LEFT_CNR, WND_FULL, WND_CNTR, BTM_RGHT_CNR, WND_FULL_ALMST, RESET,
+        PRV_SPACE, NXT_SPACE, KC_NO, KC_NO, KC_NO, KC_NO
+    );
+
+    uint32_t word_layer[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, EM_DASH, KC_NO, COMP_MODE, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_TRNS, KC_NO, EN_DASH, KC_NO, PASTE_STYLE,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
+        KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_DEL
+    );
+
+    uint32_t fkeys_layer[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F12, KC_F7, KC_F8, KC_F9, KC_F17, KC_F20,
+        KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_F11, KC_F4, KC_F5, KC_F6, KC_F16, KC_F19,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F10, KC_F1, KC_F2, KC_F3, KC_F13, KC_F18,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    );
+
+    uint32_t numpad_layer[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_PAST, KC_P7, KC_P8, KC_P9, KC_PSLS, KC_PEQL,
+        KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PPLS, KC_P4, KC_P5, KC_P6, KC_PMNS, KC_PERC,
+        KC_LALT, KC_LGUI, KC_NO, KC_NO, KC_NO, KC_NO,  KC_P0, KC_P1, KC_P2, KC_P3, KC_PDOT, KC_PENT,
+        KC_NO, KC_TRNS, KC_NO, KC_NO, KC_LPRN, KC_RPRN
+    );
+
+    /*
+     * add the other layers
+     */
+    for (int row = 0; row < MATRIX_ROWS; ++row)
+    {
+        for (int col = 0; col < MATRIX_COLS; ++col)
+        {
+            matrix[row][col].addActivation(_QWERTY, Method::MT_TAP,  qwerty_taph[row][col]);
+            matrix[row][col].addActivation(_QWERTY, Method::MT_HOLD, qwerty_hold[row][col]);
+            matrix[row][col].addActivation(_QWERTY, Method::DT_TAP,  qwerty_tap[row][col]);
+            matrix[row][col].addActivation(_QWERTY, Method::DT_DOUBLETAP, qwerty_dtap[row][col]);
+
+            matrix[row][col].addActivation(_SYM, Method::PRESS, symbol_layer[row][col]);
+            matrix[row][col].addActivation(_NUM, Method::PRESS, num_layer[row][col]);
+            matrix[row][col].addActivation(_NAV, Method::PRESS, nav_layer[row][col]);
+            matrix[row][col].addActivation(_MEDIA, Method::PRESS, media_layer[row][col]);
+            matrix[row][col].addActivation(_WORD, Method::PRESS, word_layer[row][col]);
+        }
+    }
+
+}
+
+
---
+++ firmware/keyboards/opus/keymaps/default/keymap.h
@@ -0,0 +1,153 @@
+/*
+Copyright 2018 <Pierre Constantineau>
+
+3-Clause BSD License
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+#include <stdint.h>
+#include "hid_keycodes.h"
+#include "keyboard_config.h"
+#include "advanced_keycodes.h"
+#include "Key.h"
+#include <array>
+
+#ifndef KEYMAP_H
+#define KEYMAP_H
+
+#define KC_CAP_D MOD(MOD_LSHIFT, KC_D)
+
+// Layer Definitions
+
+#define _QWERTY 0
+#define _SYM    1
+#define _NUM    2
+#define _NAV    3
+#define _MEDIA  4
+#define _WORD   5
+
+// Layer Keycode Definitions
+
+#define L_QWERTY  (LAYER_0 + _QWERTY)
+#define L_SYM     (LAYER_0 + _SYM)
+#define L_NUM     (LAYER_0 + _NUM)
+#define L_NAV     (LAYER_0 + _NAV)
+#define L_MEDIA   (LAYER_0 + _MEDIA)
+#define L_WORD    (LAYER_0 + _WORD)
+
+// Mod Combos
+#define SGUI(KEY) LSFT(LGUI(KEY))
+#define LCA(KEY) LCTL(LALT(KEY)) 
+#define MEH(KEY) LSFT(LCTL(LALT(KEY)))
+#define LCAG(KEY) LGUI(LCTL(LALT(KEY)))
+
+// Misc
+#define CTL_C LCTL(KC_C)
+#define CTL_GRV LCTL(KC_GRV)
+#define PRV_WORD LALT(KC_LEFT)
+#define NXT_WORD LALT(KC_RGHT)
+#define PRV_TAB SGUI(KC_LBRC)
+#define NXT_TAB SGUI(KC_RBRC)
+#define ZOOM_OUT LGUI(KC_MINS)
+#define ZOOM_IN LGUI(KC_PLUS)
+#define NXT_WND LGUI(KC_GRV)
+#define PRV_SPACE LCTL(LSFT(KC_TAB))
+#define NXT_SPACE LCTL(KC_TAB)
+
+// Screenshots
+#define SCRN SGUI(KC_5)
+#define SCRN_WHL SGUI(KC_3)
+#define SCRN_SLCT SGUI(KC_4)
+#define SCRN_SLCT_CP MEH(KC_4)
+
+// Window Management
+#define UP_LEFT_CNR LCA(KC_U)
+#define UP_RGHT_CNR LCA(KC_I)
+#define BTM_LEFT_CNR LCA(KC_J)
+#define BTM_RGHT_CNR LCA(KC_K)
+#define WND_LEFT LCA(KC_LEFT)
+#define WND_RGHT LCA(KC_RGHT)
+#define WND_DOWN LCA(KC_DOWN)
+#define WND_LRG LCA(KC_EQL)
+#define WND_SML LCA(KC_MINS)
+#define WND_UP LCA(KC_UP)
+#define WND_CNTR LCA(KC_C)
+#define WND_FULL LCA(KC_ENT)
+#define WND_FULL_ALMST MEH(KC_UP)
+#define PRV_SCRN LCAG(KC_LEFT)
+#define NXT_SCRN LCAG(KC_RGHT)
+
+// OS Functions
+#define SLEEP LALT(LGUI(KC_POWER))
+#define KC_MPLY KC_MEDIA_PLAY_PAUSE
+#define KC_MPRV KC_MEDIA_PREV_TRACK
+#define KC_MNXT KC_MEDIA_NEXT_TRACK
+#define KC_BRIU KC_DISPLAY_BRIGHTI
+#define KC_BRID KC_DISPLAY_BRIGHTD
+
+// Grammar
+#define EM_DASH LALT(LSFT(KC_MINS))
+#define EN_DASH LALT(KC_MINS)
+#define COMP_MODE LALT(LGUI(KC_F))
+#define PASTE_STYLE SGUI(LALT(KC_V))
+
+// tap/hold definitions
+
+// MT(MOD_LCTL, KC_ESC) 
+#define CTRLESC XXXXXXX
+#define CTRLESC_TAP KC_ESC
+#define CTRLESC_HOLD KC_LCTL
+
+// MT(MOD_LGUI, KC_Z) 
+#define GUIZ XXXXXXX
+#define GUIZ_TAP KC_Z
+#define GUIZ_HOLD KC_LGUI
+
+// MT(MOD_LGUI, KC_SLSH) 
+#define GUISLSH XXXXXXX
+#define GUISLSH_TAP KC_SLSH
+#define GUISLSH_HOLD KC_LGUI
+
+// MT(MOD_LALT, KC_TAB) 
+#define ALTTAB XXXXXXX
+#define ALTTAB_TAP KC_TAB
+#define ALTTAB_HOLD KC_LALT
+
+// LT(_SYM, KC_SPC) 
+#define SYMSPC XXXXXXX
+#define SYMSPC_TAP KC_SPC
+#define SYMSPC_HOLD L_SYM
+// LT(_MEDIA, KC_QUOT) 
+#define MEDIAQUOT XXXXXXX
+#define MEDIAQUOT_TAP KC_QUOT
+#define MEDIAQUOT_HOLD L_MEDIA
+// LT(_MEDIA, KC_TAB) 
+#define MEDIATAB XXXXXXX
+#define MEDIATAB_TAP KC_TAB
+#define MEDIATAB_HOLD L_MEDIA
+// LT(_NUM, KC_F) 
+#define NUMF XXXXXXX
+#define NUMF_TAP KC_F
+#define NUMF_HOLD L_NUM
+
+// ACTION_TAP_DANCE_DOUBLE(KC_MPLY, KC_F6),
+#define TD_MPLY_DND  XXXXXXX
+#define TD_MPLY_DND_TAP  KC_MPLY
+#define TD_MPLY_DND_DTAP  KC_F6
+
+void setupKeymap();
+extern std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix;
+
+#endif /* KEYMAP_H */
